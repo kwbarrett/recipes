f8c52656b2647f196d18f05b4686d9065341841b
Kenneth Barrett <hamptonalumkb@proton.me>
2023-02-04 10:13:14 -0500

    Built out recipe-list component
    Used the recipe model in the recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe( 'A Test Recipe', 'This is simply a test', 'https://i0.wp.com/www.honeybearlane.com/wp-content/uploads/2015/06/e11a57dc1244fe3703e1b803d927d4e2.jpg?fit=600%2C900&ssl=1' ),
+    new Recipe( 'A Test Recipe', 'This is simply a test', 'https://i0.wp.com/www.honeybearlane.com/wp-content/uploads/2015/06/e11a57dc1244fe3703e1b803d927d4e2.jpg?fit=600%2C900&ssl=1' )
+  ];
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
 
 }
